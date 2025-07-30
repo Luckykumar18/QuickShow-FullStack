a13b9acf62c7a8424f95cf4a3e21074dff52b078
Lucky Kumar <luckykumat303@gmail.com>
2025-07-30 11:54:34 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}`)
+         mongoose.connection.on('error', (err) =>
+      console.log('❌ MongoDB error:', err)
+    );
+        await mongoose.connect(process.env.MONGODB_URI);
+        
     } catch (error) {
         console.log(error.message);
         
